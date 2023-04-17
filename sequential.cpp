45941645e896e99bd082d02ffa5fba386898a732
SUMAN TANDAN <sumantandan1999@gmail.com>
2023-04-17 19:40:15 +0530

    file added

---
+++ sequential.cpp
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <omp.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+string substring(int start, int length, string strword) {
+    string a = "";
+    for (int i = start; i < length; i++) {
+        a += strword[i];
+    }
+    return a;
+}
+
+int find_all(string sen, string word) {
+    int wordLen = word.length();
+    int start = 0;
+    int endword = wordLen;
+    int finLength = sen.length();
+    int count = 0;
+    string senWord = "";
+    for (int i = 0; i <= sen.length(); i++) {
+        if (endword > finLength) {
+            break;
+        }
+        senWord = substring(start, endword, sen);
+        if ((senWord.compare(word)) == 0) {
+            count++;
+            if (endword > finLength) {
+                break;
+            }
+        }
+        start += 1;
+        endword += 1;
+    }
+    return count;
+}
+
+int display(string path, string word_to_search) {
+    int totalCount = 0;
+    string line;
+    int count = 1;
+    ifstream myfile(path);
+    if (myfile.is_open()) {
+        while (getline(myfile, line)) {
+            totalCount += find_all(line, word_to_search);
+            count++;
+        }
+        myfile.close(); // close the file after use
+    }
+    else {
+        cout << "File not open\n" << endl;
+    }
+    return totalCount;
+}
+
+int main() {
+    string Path = "C:/xampp/htdocs/parallel/"; // use forward slash instead of backslash in the path
+    string word_to_search = "";
+    cout << "Enter a word to search: ";
+    cin >> word_to_search;
+    double time = omp_get_wtime();
+    for (int i = 1; i <= 5; i++) {
+        string npath = "File" + to_string(i) + ".txt";
+        cout << "Total Count is " + to_string(display(Path + npath, word_to_search)) << " from file " + to_string(i) << endl;
+    }
+    time = omp_get_wtime() - time;
+    cout << "Time is " + to_string(time);
+    return 0;
+}
